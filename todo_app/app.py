17f6be7d3da86841f154ee90accecf7371cb2f8a
RhiMcG <rtjmcgill@gmail.com>
2022-09-06 17:39:05 +0100

    Use view model

--- todo_app/app.py
+++ todo_app/app.py
@@ -6,11 +6,25 @@ from todo_app.flask_config import Config
 app = Flask(__name__)
 app.config.from_object(Config())
 
+class ViewModel:
+  def __init__(self, todo_items, done_items):
+    self._todo_items = todo_items
+    self._done_items = done_items
+
+  @property
+  def todo_items(self):
+      return self._todo_items
+
+  @property 
+  def done_items(self):  
+    return self._done_items
+
 
 @app.route('/')
 def get_todos():
     [todo_items, done_items] = get_items()
-    return render_template('index.html', todo_items = todo_items, done_items=done_items)
+    item_view_model = ViewModel(todo_items, done_items)
+    return render_template('index.html', view_model = item_view_model)
 
 @app.route('/add-todo', methods=['POST'])
 def add_todo():
